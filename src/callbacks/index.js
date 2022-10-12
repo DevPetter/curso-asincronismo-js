cd2075d0d030a60c98f197a74fb6f1582fbc1172
petter <petter0915@gmail.com>
2022-10-11 22:20:08 -0500

    Explicación funciones de un callback

--- src/callbacks/index.js
+++ src/callbacks/index.js
@@ -1,10 +1,12 @@
+//declaración de función sumar
 function sum(num1, num2){
     return num1 + num2
 }
 
+//Creación de un callback
 function calcular(num1,num2,callb){
     return callb(num1,num2)
 }
 
-console.log(calcular(2,4,sum))
+console.log(calcular(2,4,sum)) //Implementación de un callback
 
